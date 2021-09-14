fd8ab98bb882d6ae3fd3c78a22be70528d6c899a
Joanna Lu <jkl105@ucsd.edu>
2021-09-13 21:22:28 -0700

    hamburger menu

---
+++ myScript.js
@@ -0,0 +1,8 @@
+function menu() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += "responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
